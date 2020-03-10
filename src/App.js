4a5692696d76ca3965736d479c052b705133727b
Andrea  Piazza <apiazza023@gmail.com>
2020-03-09 21:26:33 -0700

    Added FontAwesome & reusable components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+
 function App() {
-  // tom state - what our current state is 
-  // set tom state - what we want our state to be 
-
-  const [tomState, setTomState] = React.useState({
-    age: 0, 
-    light: 0,
-    hunger: 0,
-  })
-
   return (
     <div className="App">
       <div className="egg"></div>
       <Screen />
-
-      <Button id="feedButton" onClick={()=>{
-        setTomState({
-          ...tomState,
-          hunger: tomState.hunger - 1,
-        })
-        alert(tomState.age)
-      }}/>
+      <CreateButton type="bathroom" />
     </div>
   );
 }
 
 function Screen() {
-  return <div className="screen">Screen</div>;
+  const [hungerState, setHunger] = React.useState({
+    feed: 0, 
+  })
+
+  return <div className="screen">
+  <div>
+    <FeedButton onClick={(e) => {
+      setHunger({
+        ...hungerState, 
+        feed: hungerState.feed + 1,
+      })
+      alert(hungerState.feed)
+    }}> Feed</FeedButton>
+  </div>
+  </div>;
 }
 // Every component takes an object to access properties
-function Button({ onClick }) {
+function FeedButton({ onClick }) {
   return (
     <button
       onClick={() => {
         console.log('Clicking button:', )
         onClick()
       }}
-    >Feed</button>
-  );
+    ><i class="fa fa-cutlery" aria-hidden="true"></i>
+    </button>
+  )
 }
 
+function CreateButton(){
+  return (
+      <button onClick={() => {
+        
+      }}>
+        <i class="fa fa-bath" aria-hidden="true"></i>
+      </button>
+  )
+}
 
 export default App;
